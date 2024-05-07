3488cb81765429c9b88f3970d55a0e329f6ac24e
Marcel <marcel.schmid@students.ffhs.ch>
2024-05-07 15:10:56 +0200

    Provider, Api Zugriff und ListView

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,74 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(const MyApp());
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider<PostProvider>(create: (_) => PostProvider())
+    ],
+    child: MyApp(),
+  ));
+}
+
+//MODELS
+class Post {
+  int id = 0;
+  int userId = 0;
+  String title = "";
+  String body = "";
+
+  Post({this.id = 0, this.userId = 0, this.title = "", this.body = ""});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      id: json['id'],
+      userId: json['userId'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+}
+
+//SERVICE
+class ApiService {
+  Future<List<Post>> getPosts(context) async {
+    late List<Post> posts = [];
+    try {
+      final response = await http.get(
+        Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+      );
+      if (response.statusCode == 200) {
+        final body = json.decode(response.body);
+        //final List list = json.decode(body['data']);
+        //final list = body['data'].map<Post>((e) => Post.fromJson(e)).toList();
+        posts = body.map<Post>((e)=> Post.fromJson(e)).toList();
+       // posts.addAll(list);
+      } else {
+        print('Error Occurred');
+      }
+    } catch (e) {
+      print('Error Occurred' + e.toString());
+    }
+    return posts;
+  }
+}
+
+//PROVIDER
+class PostProvider with ChangeNotifier {
+  late List<Post> posts = [];
+
+  bool loading = false;
+  ApiService apiService = ApiService();
+  getData(context) async {
+    loading = true;
+    posts = await apiService.getPosts(context);
+    loading = false;
+
+    notifyListeners();
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -31,13 +98,13 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const PostListView(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class PostListView extends StatefulWidget {
+  const PostListView({super.key, required this.title});
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -51,75 +118,60 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<PostListView> createState() => _PostListViewState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _PostListViewState extends State<PostListView> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final postProvider = Provider.of<PostProvider>(context);
+
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          // TRY THIS: Try changing the color here to a specific color (to
+          // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+          // change color while the other colors stay the same.
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body: Consumer<PostProvider>(
+          builder: (context, value, child) {
+            // If the loading it true then it will show the circular progressbar
+
+            if (value.loading || value.posts.isEmpty) {
+              value.getData(context);
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            // If loading is false then this code will show the list of todo item
+            final articles = value.posts;
+            return buildPosts(articles, 1);
+          },
+        ));
+  }
+
+  ListView buildPosts(List<Post> posts, int categoryId) {
+    return ListView.builder(
+      // Providing a restorationId allows the ListView to restore the
+      // scroll position when a user leaves and returns to the app after it
+      // has been killed while running in the background.
+      restorationId: 'postListView_$categoryId',
+      itemCount: posts.length,
+      itemBuilder: (BuildContext context, int index) {
+        final item = posts[index];
+
+        return Card(
+          child: Column(children: [
+            ListTile(
+              title: Text("${item.title}"),
+              subtitle: Text("${item.body}"),
+            ),
+          ]),
+        );
+      },
     );
   }
 }
